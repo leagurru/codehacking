43124df1683e5062952bb24bd917d1dfae22fabc
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-07 18:36:55 -0300

    Clase 227

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,4 +2,39 @@
 @section('content')
     <h1>Crear  un Posteo</h1>
 
+
+        {!! Form::open(['method'=>'POST','action'=>'AdminPostsController@store','files'=>true])  !!}
+        {{csrf_field()}}
+
+        <div class="form-group">
+
+            {!! Form::label('title','Título:') !!}
+            {!! Form::text('title',null,['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('category_id','Categoría:') !!}
+            {!! Form::select('category_id',array(''=>'Opciones'),null,['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('photo_id','Foto:') !!}
+            {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
+        </div>
+
+         <div class="form-group">
+             {!! Form::label('body','Texto:') !!}
+             {!! Form::textarea('body',null,['class'=>'form-control']) !!}
+         </div>
+
+
+        <div class="form-group">
+            {!! Form::submit('Crear Post',['class'=>'btn btn-primary']) !!}
+        </div>
+
+
+        {!! Form::close() !!}
+
+
+
 @stop
