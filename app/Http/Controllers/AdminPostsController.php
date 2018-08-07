3f03203a2e7fee3850248fcc20bf804c4ecae4ad
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-07 19:26:45 -0300

    Clase 230 y 231

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PostsCreateRequest;
+use App\Photo;
 use App\Post;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
 
 class AdminPostsController extends Controller
 {
@@ -40,8 +42,20 @@ class AdminPostsController extends Controller
     public function store(PostsCreateRequest $request)
     {
 
+        $input = $request->all();
         $user = Auth::user();
 
+        if($file = $request->file('photo_id')){
+
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->posts()->create($input);
+
+        return redirect('admin/posts');
 
 //        return $request->all();
     }
