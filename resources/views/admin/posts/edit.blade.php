2c087270d670d4ebe8885e5dc7442f71c8cff006
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-08 17:35:32 -0300

    Clase 242

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,48 +2,58 @@
 @section('content')
     <h1>Editar Posteo</h1>
 
+
     <div class="row">
-        {!! Form::model($post,['method'=>'PATCH','action'=>['AdminPostsController@update',$post->id],'files'=>true])  !!}
-        {{csrf_field()}}
-
-        <div class="form-group">
-
-            {!! Form::label('title','Título:') !!}
-            {!! Form::text('title',null,['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('category_id','Categoría:') !!}
-            {{--{!! Form::select('category_id',array(1=>'PHP',2=>'Javascrip'),null,['class'=>'form-control']) !!}--}}
-            {!! Form::select('category_id', $categories ,null,['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('photo_id','Foto:') !!}
-            {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('body','Texto:') !!}
-            {!! Form::textarea('body',null,['class'=>'form-control']) !!}
-        </div>
 
 
-        <div class="form-group">
-            {!! Form::submit('Actualizar Post',['class'=>'btn btn-primary col-sm-6']) !!}
-        </div>
+            <div class="col-sm-3">
 
+                <img src="{{$post->photo->file}}" alt="" class="img-responsive">
 
-        {!! Form::close() !!}
-
-
-        {!! Form::open(['method'=>'DELETE','action'=>['AdminPostsController@destroy',$post->id]])  !!}
-            <div class="form-group">
-                {!! Form::submit('Borrar Post',['class'=>'btn btn-danger col-sm-6']) !!}
             </div>
 
-        {!! Form::close() !!}
+            <div class="col-sm-9">
+                {!! Form::model($post,['method'=>'PATCH','action'=>['AdminPostsController@update',$post->id],'files'=>true])  !!}
+                {{csrf_field()}}
 
+                <div class="form-group">
+
+                    {!! Form::label('title','Título:') !!}
+                    {!! Form::text('title',null,['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('category_id','Categoría:') !!}
+                    {{--{!! Form::select('category_id',array(1=>'PHP',2=>'Javascrip'),null,['class'=>'form-control']) !!}--}}
+                    {!! Form::select('category_id', $categories ,null,['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('photo_id','Foto:') !!}
+                    {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('body','Texto:') !!}
+                    {!! Form::textarea('body',null,['class'=>'form-control']) !!}
+                </div>
+
+
+                <div class="form-group">
+                    {!! Form::submit('Actualizar Post',['class'=>'btn btn-primary col-sm-6']) !!}
+                </div>
+
+
+                {!! Form::close() !!}
+
+
+                {!! Form::open(['method'=>'DELETE','action'=>['AdminPostsController@destroy',$post->id]])  !!}
+                    <div class="form-group">
+                        {!! Form::submit('Borrar Post',['class'=>'btn btn-danger col-sm-6']) !!}
+                    </div>
+
+                {!! Form::close() !!}
+            </div>
     </div>
 
     <div class="row">
