e042dadd0984f738dbcb1892e7d314c8b8e4205e
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-08 17:05:54 -0300

    Clase 240

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -119,6 +119,9 @@ class AdminPostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::findOrFail($id);
+        unlink(public_path('' . $post->photo->file));
+        $post->delete();
+        return redirect('/admin/posts');
     }
 }
