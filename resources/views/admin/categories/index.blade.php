0594b52f70e64fc5ad532f34a007eeefb233f37e
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-08 18:25:21 -0300

    Clase 245

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -3,4 +3,44 @@
 
 <h1>Categorías</h1>
 
+    <div class="col-sm-6">
+        {!! Form::open(['method'=>'POST','action'=>'AdminCategoriesController@store'])  !!}
+
+             <div class="form-group">
+                 {!! Form::label('name','Nombre:') !!}
+                 {!! Form::text('name',null,['class'=>'form-control']) !!}
+             </div>
+
+             <div class="form-group">
+                 {!! Form::submit('Crear Categoría',['class'=>'btn btn-primary']) !!}
+             </div>
+
+        {!! Form::close() !!}
+    </div>
+
+    <div class="col-sm-6">
+        @if($categories)
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Fecha creación</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                @foreach($categories as $category)
+                      <tr>
+                        <td>{{$category->id}}</td>
+                        <td>{{$category->name}}</td>
+                        <td>{{$category->created_at ? $category->created_at->diffForHumans() : 'Sin fecha'}}</td>
+                      </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
+
+
 @stop
