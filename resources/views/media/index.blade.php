ddf4c889cd60e0378a18a0e25a79db5577fcaa29
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-08 22:33:55 -0300

    Clase 246 parcial

---
+++ resources/views/media/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin')
+@section('content')
+
+    <h1>Media</h1>
+
+    @if($photos)
+
+        <table class="table">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Archivo</th>
+                <th>Creado</th>
+              </tr>
+            </thead>
+
+            <tbody>
+            @foreach($photos as $photo)
+              <tr>
+                <td>{{$photo->id}}</td>
+                <td>{{$photo->file}}</td>
+                <td>{{$photo->created_at ? $photo->created_at : 'Sin fecha'}}</td>
+              </tr>
+
+            @endforeach
+            </tbody>
+
+        </table>
+
+    @endif
+@stop
