01c91d288dc9b703ddaa12c57c7da309e0108687
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-13 17:44:08 -0300

    Clase 259

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -23,6 +23,47 @@
               <td>{{$comment->email}}</td>
               <td>{{$comment->body}}</td>
               <td><a href="{{route('home.post', $comment->post->id)}}">Ver Post</a></td>
+
+              <td>
+                  @if($comment->is_active == 1)
+
+                      {!! Form::open(['method'=>'PATCH','action'=>['PostCommentsController@update', $comment->id]])  !!}
+                      {{--{{csrf_field()}}--}}
+
+                        <input type="hidden" name="is_active" value="0">
+                        <div class="form-group">
+                            {!! Form::submit('Desaprobar',['class'=>'btn btn-success']) !!}
+                        </div>
+
+                      {!! Form::close() !!}
+                  @else
+
+                      {!! Form::open(['method'=>'PATCH','action'=>['PostCommentsController@update', $comment->id]])  !!}
+                      {{--{{csrf_field()}}--}}
+
+                        <input type="hidden" name="is_active" value="1">
+
+
+                        <div class="form-group">
+                          {!! Form::submit('Aprobar',['class'=>'btn btn-info']) !!}
+                        </div>
+
+                      {!! Form::close() !!}
+
+                  @endif
+
+
+              </td>
+              <td>
+                  {!! Form::open(['method'=>'DELETE','action'=>['PostCommentsController@destroy', $comment->id]])  !!}
+                  {{--{{csrf_field()}}--}}
+
+                      <div class="form-group">
+                          {!! Form::submit('Borrar',['class'=>'btn btn-danger']) !!}
+                      </div>
+
+                  {!! Form::close() !!}
+              </td>
           </tr>
 
         @endforeach
