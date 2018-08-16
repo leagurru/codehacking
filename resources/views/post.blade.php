651cbb8c5c7becfe443a9a1485246a316ddade0d
leagurru <leagurru@gmail.com>
2018-08-16 13:20:01 -0300

    Clase 266 parcial, hasta 5'

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -93,38 +93,45 @@
                 </h4>
                 <p>{{$comment->body}}</p>
 
+
+                @if(count($comment->replies) > 0 )
+                    @foreach($comment->replies as $reply)
                 <!-- Nested Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Nested Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">Nested Start Bootstrap
+                                <small>August 25, 2014 at 9:30 PM</small>
+                            </h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+
+                        {!! Form::open(['method'=>'POST','action'=>'CommentRepliesController@createReply'])  !!}
+                            {{csrf_field()}}
+
+                            <div class="form-group">
+
+                                <input type="hidden" name="comment_id" value="{{$comment->id}}">
+
+                            {!! Form::label('body','Body:') !!}
+                            {!! Form::textarea('body',null,['class'=>'form-control','rows'=>1]) !!}
+                            </div>
+
+                            <div class="form-group">
+                            {!! Form::submit('submit',['class'=>'btn btn-primary']) !!}
+                            </div>
+
+                        {!! Form::close() !!}
+
+
                     </div>
-
-                    {!! Form::open(['method'=>'POST','action'=>'CommentRepliesController@createReply'])  !!}
-                        {{csrf_field()}}
-
-                        <div class="form-group">
-
-                            <input type="hidden" name="comment_id" value="{{$comment->id}}">
-
-                        {!! Form::label('body','Body:') !!}
-                        {!! Form::textarea('body',null,['class'=>'form-control','rows'=>1]) !!}
-                        </div>
-
-                        <div class="form-group">
-                        {!! Form::submit('submit',['class'=>'btn btn-primary']) !!}
-                        </div>
-
-                    {!! Form::close() !!}
-
-
-                </div>
                 <!-- End Nested Comment -->
+                @endforeach
+                @endif
+
+
 
 
             </div>
