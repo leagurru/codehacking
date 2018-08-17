819e293ba184bf4e8e62c72af1e09c6c94b279d5
leagurru <leagurru@gmail.com>
2018-08-17 10:26:32 -0300

    Clase 266 finalizada

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -97,15 +97,15 @@
                 @if(count($comment->replies) > 0 )
                     @foreach($comment->replies as $reply)
                 <!-- Nested Comment -->
-                    <div class="media">
+                    <div id="nested-comment" class="media">
                         <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                            <img height="64" class="media-object" src="{{$reply->photo}}" alt="">
                         </a>
                         <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
+                            <h4 class="media-heading">{{$reply->author}}
+                                <small>{{$reply->created_at ? $reply->created_at->diffForHumans(): 'Sin fecha'}}</small>
                             </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                            <p>{{$reply->body}}</p>
                         </div>
 
                         {!! Form::open(['method'=>'POST','action'=>'CommentRepliesController@createReply'])  !!}
