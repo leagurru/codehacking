c5a358d503840a6a9b012844bd9cee9dbafb8729
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-18 18:07:39 -0300

    Clase 278 y 279 laravel 5.3 update ok fixing errores

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,22 +26,20 @@ Route::get('post/{id}', ['as'=>'home.post', 'uses'=> 'AdminPostsController@post'
 //   return view('admin.index');
 //});
 
-Route::group(['middleware'=>'admin'], function(){
-
-    Route::get('/admin', function(){
-        return view('admin.index');
-    });
-
-    Route::resource('admin/users','AdminUsersController');
-    Route::resource('admin/posts','AdminPostsController');
-    Route::resource('admin/categories','AdminCategoriesController');
-    Route::resource('admin/media','AdminMediasController');
-    Route::resource('admin/comments','PostCommentsController');
-    Route::resource('admin/comment/replies','CommentRepliesController');
-
-    //Route::get('admin/media/upload', ['as'=> 'admin.media.upload', 'uses'=> 'AdminMediasController@store']);
-
-});
+//Route::group(['middleware'=>'admin'], function(){
+//
+//    Route::get('/admin', function(){
+//        return view('admin.index');
+//    });
+//
+//    Route::resource('admin/users','AdminUsersController');
+//    Route::resource('admin/posts','AdminPostsController');
+//    Route::resource('admin/categories','AdminCategoriesController');
+//    Route::resource('admin/media','AdminMediasController');
+//    Route::resource('admin/comments','PostCommentsController');
+//    Route::resource('admin/comment/replies','CommentRepliesController');
+//    //Route::get('admin/media/upload', ['as'=> 'admin.media.upload', 'uses'=> 'AdminMediasController@store']);
+//});
 
 
 Route::group(['middleware'=>'auth'], function(){
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,31 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+Auth::routes();
+
+Route::get('/logout', 'Auth\LoginController@logout');
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('post/{id}', ['as'=>'home.post', 'uses'=> 'AdminPostsController@post']);
+
+Route::group(['middleware'=>'admin'], function(){
+
+    Route::get('/admin', function(){
+        return view('admin.index');
+    });
+
+    Route::resource('admin/users','AdminUsersController');
+    Route::resource('admin/posts','AdminPostsController');
+    Route::resource('admin/categories','AdminCategoriesController');
+    Route::resource('admin/media','AdminMediasController');
+    Route::resource('admin/comments','PostCommentsController');
+    Route::resource('admin/comment/replies','CommentRepliesController');
+    //Route::get('admin/media/upload', ['as'=> 'admin.media.upload', 'uses'=> 'AdminMediasController@store']);
 });
