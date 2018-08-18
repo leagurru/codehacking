48948277f1c0a7558f6b7b1d2ee58e4ada48f37d
Leandro Gurruchaga <leagurru@gmail.com>
2018-08-18 14:01:33 -0300

    Clase 277 laravel 5.3 update ok

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -24,10 +24,15 @@ class EventServiceProvider extends ServiceProvider
      * @param  \Illuminate\Contracts\Events\Dispatcher  $events
      * @return void
      */
-    public function boot(DispatcherContract $events)
-    {
-        parent::boot($events);
+//    public function boot(DispatcherContract $events)
+//    {
+//        parent::boot($events);
+//
+//        //
+//    }
 
-        //
+    public function boot()
+    {
+        parent::boot();
     }
 }
