dd24ee9c1040cc581f1f3f41cbc6b33514b3d34c
leagurru <leagurru@gmail.com>
2018-08-27 13:55:10 -0300

    Clase 320

---
+++ resources/views/includes/admin_nav.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">Home</a>
+    </div>
+    <!-- /.navbar-header -->
+
+    <!--Admin Top Navigation-->
+    @include('includes.admin_top_nav')
+
+
+        <!-- /.dropdown -->
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+                <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                </li>
+                <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                </li>
+                <li class="divider"></li>
+                <li><a href="{{ url('/logout') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                </li>
+            </ul>
+            <!-- /.dropdown-user -->
+        </li>
+        <!-- /.dropdown -->
+
+
+    </ul>
+
+
+
+    <!--Admin Side Navigation-->
+    @include('includes.admin_side_nav')
+
+    <!-- /.navbar-static-side -->
+</nav>
